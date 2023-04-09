681925d97a394de9986fc598b46d545df020c8c7
Arctic-Xiangjian <xiangjianhou11@gmail.com>
2023-04-10 01:00:31 +0400

    init3

--- pretrain/ml710_iclr_idea.py
+++ pretrain/ml710_iclr_idea.py
@@ -7,6 +7,7 @@ from typing import List
 import random
 import numpy as np
 import os
+import wandb
 
 import torch
 from torch.nn.parallel import DistributedDataParallel
@@ -29,6 +30,8 @@ from model_and_hyperpam import (
     BATCH_SIZE,
     PATH_DATA,
     DEVICE,
+    EPOCHS,
+    WANDB_API,
     model_without_ddp
 )
 
@@ -61,7 +64,9 @@ opt_clz = {
 
 optimizer = opt_clz(params=param_groups, lr=2e-4*BATCH_SIZE/256, weight_decay=0.0)
 
-def pre_train_epochs(num_epochs, tb_lg: misc.TensorboardLogger, itrt_train, iters_train, model, optimizer, device):
+print(f'Optimizer: {optimizer}')
+
+def pre_train_epochs(num_epochs, itrt_train, iters_train, model, optimizer, device):
     model = model.to(device)
     
     for ep in range(num_epochs):
@@ -89,9 +94,14 @@ def pre_train_epochs(num_epochs, tb_lg: misc.TensorboardLogger, itrt_train, iter
             loss.backward()
             loss = loss.item()
 
+            wandb.login(key=WANDB_API)
+            run = wandb.init(project='ml710_project', entity='arcticfox', name='pretrain_ourdata'+'_'+'resnet50'+'_'+datetime.now().strftime('%Y%m%d_%H%M%S'), job_type="training",reinit=True)
+
             if not math.isfinite(loss):
                 print(f'Loss is {loss}, stopping training!', force=True, flush=True)
                 sys.exit(-1)
+            
+            wandb.log({"loss": loss})
 
             # optimize
             grad_norm = None
@@ -101,43 +111,24 @@ def pre_train_epochs(num_epochs, tb_lg: misc.TensorboardLogger, itrt_train, iter
             torch.cuda.synchronize()
 
             # log
-            me.update(last_loss=loss)
-            me.update(max_lr=max_lr)
-            tb_lg.update(loss=me.meters['last_loss'].global_avg, head='train_loss')
-            tb_lg.update(sche_lr=max_lr, head='train_hp/lr_max')
-            tb_lg.update(sche_lr=min_lr, head='train_hp/lr_min')
-            tb_lg.update(sche_wd=max_wd, head='train_hp/wd_max')
-            tb_lg.update(sche_wd=min_wd, head='train_hp/wd_min')
+            # me.update(last_loss=loss)
+            # me.update(max_lr=max_lr)
+            # tb_lg.update(loss=me.meters['last_loss'].global_avg, head='train_loss')
+            # tb_lg.update(sche_lr=max_lr, head='train_hp/lr_max')
+            # tb_lg.update(sche_lr=min_lr, head='train_hp/lr_min')
+            # tb_lg.update(sche_wd=max_wd, head='train_hp/wd_max')
+            # tb_lg.update(sche_wd=min_wd, head='train_hp/wd_min')
 
-            if grad_norm is not None:
-                me.update(orig_norm=grad_norm)
-                tb_lg.update(orig_norm=grad_norm, head='train_hp')
-            tb_lg.set_step()
+            # if grad_norm is not None:
+            #     me.update(orig_norm=grad_norm)
+            #     tb_lg.update(orig_norm=grad_norm, head='train_hp')
+            # tb_lg.set_step()
 
         print(f'Finished training epoch {ep}')
 
 
-        import datetime
-        pre_train_save_dir = '/home/chong.tian/hc701/checkpoint_pre_train/still_pre_train'
-        fine_tune_save_dir = '/home/chong.tian/hc701/checkpoint_pre_train/for_fine_tune'
-
-
-        if ep % 200 == 0:
-            torch.save(model_without_ddp.state_dict(with_config=True), os.path.join(pre_train_save_dir, f'pre_train_model_{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.pth'))
-            torch.save(model_without_ddp.sparse_encoder.sp_cnn.state_dict(), os.path.join(fine_tune_save_dir, f'fine_tune_model_{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.pth'))
 
     return {k: meter.global_avg for k, meter in me.meters.items()},model
 
 
-tb_lg = misc.TensorboardLogger('/home/chong.tian/hc701/check_tensorboard_pre_train_log', is_master=dist.is_master(), prefix='pt')
-
-
-stats,model_without_ddp_new = pre_train_epochs(1600, tb_lg, itrt_train, iters_train, model=model_without_ddp, optimizer=optimizer, device=device)
-
-import datetime
-pre_train_save_dir = '/home/chong.tian/hc701/checkpoint_pre_train/still_pre_train'
-fine_tune_save_dir = '/home/chong.tian/hc701/checkpoint_pre_train/for_fine_tune'
-
-torch.save(model_without_ddp_new.state_dict(with_config=True), os.path.join(pre_train_save_dir, f'pre_train_model_end_{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.pth'))
-torch.save(model_without_ddp_new.sparse_encoder.sp_cnn.state_dict(), os.path.join(fine_tune_save_dir, f'fine_tune_model_end_{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.pth'))
-
+stats,model_without_ddp_new = pre_train_epochs(EPOCHS, itrt_train, iters_train, model=model_without_ddp, optimizer=optimizer, device=device)
