09f22067d47006cdf70b032936e1d43427bc28d3
Arctic-Xiangjian <xiangjianhou11@gmail.com>
2023-04-11 21:54:11 +0000

    add data and model

---
+++ vit/mode_and_data_parallel.py
@@ -0,0 +1,105 @@
+import sys
+import os
+import random
+import numpy as np
+from tqdm import tqdm
+from datetime import datetime
+
+import wandb
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import timm
+
+sys.path.append("../ML710-Project")
+sys.path.append("../ML710-Project/model_and_hyperpam")
+
+from dataset.classification_dataset import (
+    train_datasets,
+)
+
+from model_and_hyperpam import (
+    SEED,
+    BATCH_SIZE,
+    EPOCHS,
+    WANDB_API,
+)
+
+from parallel.model_data_parallel_class import ViTModel_Data_Parallel
+
+if not torch.cuda.is_available():
+    raise RuntimeError('CUDA is not available')
+
+torch.cuda.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+random.seed(SEED)
+np.random.seed(SEED)
+os.environ['PYTHONHASHSEED'] = str(SEED)
+torch.manual_seed(SEED)
+
+BATCH_SIZE = BATCH_SIZE //2 *2
+EPOCHS = EPOCHS // 2
+
+device_groups = [['cuda:0','cuda:1'], ['cuda:2', 'cuda:3']]
+model_name = 'vit_large_patch16_224'
+num_classes = 5
+
+model_parallel_group1 = ViTModel_Data_Parallel(model_name, num_classes, device_groups[0])
+model_parallel_group2 = ViTModel_Data_Parallel(model_name, num_classes, device_groups[1])
+
+
+
+
+
+wandb.login(key=WANDB_API)
+run = wandb.init(project='ml710_project', entity='arcticfox', name='model_data_parallel_2+2gpu'+'_'+'_'+datetime.now().strftime('%Y%m%d_%H%M%S'), job_type="training",reinit=True)
+
+train_dataloader = DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
+# test_dataloader = DataLoader(test_datasets, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)
+
+
+def train(model_groups, train_dataloader, optimizers, criterion, num_epochs, device_groups):
+    for epoch in tqdm(range(num_epochs)):
+        this_epoch_loss = 0
+        for batch_idx, (data, target) in enumerate(train_dataloader):
+            data_group1, data_group2 = data.split(data.shape[0] // 2, dim=0)
+
+            data_group1 = data_group1.to(device_groups[0][0])
+            data_group2 = data_group2.to(device_groups[1][0])
+
+            output_group1 = model_groups[0](data_group1)
+            output_group2 = model_groups[1](data_group2)
+
+            # make output_group1 and output_group2 in the same device
+            output_group2 = output_group2.to(output_group1.device)
+            output = torch.cat([output_group1, output_group2], dim=0)
+            target = target.to(output.device)
+
+            loss = criterion(output, target)
+
+            optimizers[0].zero_grad()
+            optimizers[1].zero_grad()
+            loss.backward()
+            optimizers[0].step()
+            optimizers[1].step()
+
+        this_epoch_loss /= len(train_dataloader)
+        wandb.log({'loss': this_epoch_loss})
+
+            
+
+
+
+
+optimizer_group1 = optim.AdamW(model_parallel_group1.parameters(), lr=1e-3)
+optimizer_group2 = optim.AdamW(model_parallel_group2.parameters(), lr=1e-3)
+
+optimizers = [optimizer_group1, optimizer_group2]
+
+criterion = nn.CrossEntropyLoss()
+
+train([model_parallel_group1, model_parallel_group2], train_dataloader, optimizers, criterion, EPOCHS, device_groups)
---
+++ vit/model_parallel copy.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import random
+import numpy as np
+from tqdm import tqdm
+from datetime import datetime
+
+import wandb
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import timm
+
+sys.path.append("../ML710-Project")
+sys.path.append("../ML710-Project/model_and_hyperpam")
+
+from dataset.classification_dataset import (
+    train_datasets,
+)
+
+from model_and_hyperpam import (
+    SEED,
+    BATCH_SIZE,
+    EPOCHS,
+    WANDB_API,
+)
+
+from parallel.model_parallel_class import ViTModelParallel
+
+if not torch.cuda.is_available():
+    raise RuntimeError('CUDA is not available')
+
+torch.cuda.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+random.seed(SEED)
+np.random.seed(SEED)
+os.environ['PYTHONHASHSEED'] = str(SEED)
+torch.manual_seed(SEED)
+
+BATCH_SIZE = BATCH_SIZE //2
+EPOCHS = EPOCHS // 2
+
+device_ids = ['cuda:0', 'cuda:1']  # 在这里设置要使用的GPU设备ID
+model_name = 'vit_large_patch16_224'
+num_classes = 5
+model_parallel = ViTModelParallel(model_name, num_classes, device_ids)
+
+wandb.login(key=WANDB_API)
+run = wandb.init(project='ml710_project', entity='arcticfox', name='vit_model_parallel_2gpu'+'_'+'_'+datetime.now().strftime('%Y%m%d_%H%M%S'), job_type="training",reinit=True)
+
+train_dataloader = DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
+# test_dataloader = DataLoader(test_datasets, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)
+
+
+def train(model, train_loader, optimizer, criterion, epoch,device=device_ids):
+    model.train()
+    for ep in tqdm(range(epoch)):
+        this_epoch_loss = 0
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device[0]), target.to(device[-1])
+            optimizer.zero_grad()
+            output = model(data)
+            target = target.to(output.device)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            this_epoch_loss += loss.item()
+        this_epoch_loss /= len(train_loader)
+        wandb.log({"train_loss": this_epoch_loss})
+
+optimizer = optim.AdamW(model_parallel.parameters(), lr=1e-3)
+criterion = nn.CrossEntropyLoss()
+
+model_new = train(model_parallel, train_dataloader, optimizer, criterion, EPOCHS)
